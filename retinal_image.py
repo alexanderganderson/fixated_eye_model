de540a3574e0fc0cda73ae89a5eec1ebbb13ad5e
Alex Anderson <aga@berkeley.edu>
2015-03-17 23:50:59 -0700

    updated png producing file

--- retinal_image.py
+++ retinal_image.py
@@ -2,7 +2,7 @@ from EM_burak_class import *
 from scipy.signal import convolve2d
 
 
-emb = EMBurak(_DT = 0.003, _DC = 50.)
+emb = EMBurak(_DT = 0.005, _DC = 50.)
 emb.gen_data()
 emb.calculate_inner_products()
 
@@ -11,42 +11,49 @@ R = emb.R
 
 L_N = emb.L_N
 N_N = emb.N_N
+N_T = emb.N_T
+DT = emb.DT
 
-a = 20.
+a = 10.
 F = np.exp(- np.arange(0, 3 * a + 1) / (3 * a)) 
 F = F / np.sum(F)
+
+m = F.shape[0]
 
 
 R1 = np.zeros_like(R)
 
 for i in range(R1.shape[0]):
-    R1[i] = np.convolve(R[i], F, mode = 'same')
+    for t in range(N_T):
+        if (t + m < N_T):
+            R1[i, t:t+m] += R[i, t] * F
+        else:
+            R1[i, t:N_T] += R[i, t] * F[0: N_T - t]
 
 
 
 
-t = 50
+for t in range(50):
+    print t
+    plt.figure().suptitle('t = ' + str(1000 * DT * t) + ' ms') #+ '\n' +
+    #                        'Lambda0,Lambda1 = (' + str(emb.L0) + ',' + str(emb.L1) + ')'
+    #                        )
 
-plt.figure().suptitle('Diffusion constant ' + str(emb.DC) + '\n' +
-                        'Lambda0,Lambda1 = (' + str(emb.L0) + ',' + str(emb.L1) + ')'
-                        )
-plt.subplot(2, 2, 1)
-plt.imshow(emb.Ips[0, :, t].reshape(L_N, L_N), 
-           interpolation = 'nearest', cmap = plt.cm.gray_r)
-plt.title('Retinal Projection of the Image')
+    plt.subplot(2, 2, 1)
+    plt.imshow(emb.Ips[0, :, t].reshape(L_N, L_N), 
+               interpolation = 'nearest', cmap = plt.cm.gray_r)
+    plt.title('Retinal Projection of the Image')
 
-plt.subplot(2, 2, 2)
-plt.imshow(R[:, t].reshape(L_N, L_N),
-            interpolation = 'nearest', cmap = plt.cm.gray_r)
-plt.title('Spikes')
+    plt.subplot(2, 2, 2)
+    plt.imshow(R[:, t].reshape(L_N, L_N),
+                interpolation = 'nearest', cmap = plt.cm.gray_r)
+    plt.title('Spikes')
 
-plt.subplot(2, 2, 3)
-plt.imshow(R1[:, t].reshape(L_N, L_N),
-            interpolation = 'nearest', cmap = plt.cm.gray_r)
-plt.title('Exponential Moving Average of Spikes')
-#plt.colorbar()
-plt.savefig('img.png', dpi = 400)
-#plt.show()
+    plt.subplot(2, 2, 3)
+    plt.imshow(R1[:, t].reshape(L_N, L_N),
+                interpolation = 'nearest', cmap = plt.cm.gray_r)
+    plt.title('Exponential Moving Average of Spikes')
+    plt.savefig('img/img' + str(1000 + t) + '.png', dpi = 75)
 
 
 
