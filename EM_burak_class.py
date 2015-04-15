1908a485893f9ea5568ca0cef5ba66ee799f34af
Alex Anderson <aga@berkeley.edu>
2015-04-14 17:43:49 -0700

    another run

--- EM_burak_class.py
+++ EM_burak_class.py
@@ -620,7 +620,7 @@ class EMBurak:
 
 
 if __name__ == '__main__':
-    emb = EMBurak(_DC = 50., _DT = 0.001, _N_T = 200)
+    emb = EMBurak(_DC = 50., _DT = 0.001, _N_T = 100)
     emb.gen_data()
     emb.run_EM()
     emb.save()
