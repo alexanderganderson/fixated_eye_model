f79f9e3fdcee2364d96617b6d5613faa68cefa47
Alex Anderson <aga@berkeley.edu>
2015-04-15 22:39:32 -0700

    changed plot_SNR.py into a class

--- plot_SNR.py
+++ plot_SNR.py
@@ -1,79 +1,102 @@
 import numpy as np
 import cPickle as pkl
-import matplotlib.pyplot as plt
-
-data_dir = 'output/'
-
-data = pkl.load(open(data_dir + 'data.pkl', 'rb'))
-
-XR = data['XR'][0]
-YR = data['YR'][0]
-S = data['S']
-
-# Convert retinal positions to grid
-XS = data['XS']
-YS = data['YS']
-
-XS, YS = np.meshgrid(XS, YS)
-XS = XS.ravel()
-YS = YS.ravel()
-
-N_itr = data['N_itr']
-var = data['Var'][0]
-
-
-def calc_dx_dy(q):
-    """
-    Find dx, dy corresponding to iteration q
-    """
-    
-
-
+import sys
+#import matplotlib.pyplot as plt
 
 def inner_product(p1, l1x, l1y, 
                   p2, l2x, l2y, var):
     """
     Calculate the inner product between two images with the representation:
-    p1 -> pixels
-    l1 -> location of pixels
+    p1 -> array of pixels
+    l1x -> array of pixel x coordinate
+    l2x -> array of pixel y coordinate
     each pixel is surrounded by a gaussian with variance var
     """
     N = l1x.shape[0]
     l1x = l1x.reshape(N, 1)
     l2x = l2x.reshape(1, N)
-    
+
     l1y = l1y.reshape(N, 1)
     l2y = l2y.reshape(1, N)
-    
-    coupling = np.exp(-( (l1x - l2x) ** 2 + (l1y - l2y) ** 2) / (4 * var))
-    
+
+    coupling = np.exp(-( (l1x - l2x) ** 2 + 
+                         (l1y - l2y) ** 2) / (4 * var))
+
     return np.einsum('i,j,ij->', p1, p2, coupling)
-    
-    #return coupling
+
 
 def SNR(p1, l1x, l1y, p2, l2x, l2y, var):
+    """
+    Using the inner product defined above, calculates the SNR
+        between two images given in sum of gaussian form
+    See inner product for definitinos of variables
+    Note the first set of pixels and pixel locations is 
+        considered to be the ground truth
+    """
     ip12 = inner_product(p1, l1x, l1y, p2, l2x, l2y, var)            
     ip11 = inner_product(p1, l1x, l1y, p1, l1x, l1y, var)
     ip22 = inner_product(p2, l2x, l2y, p2, l2x, l2y, var)
 
     return ip11 / (ip11 + ip22 - 2 * ip12)
 
-def SNR_idx(q):
-    XY_est = data['EM_data'][q]['path_means']
-    S_est = data['EM_data'][q]['image_est']
-    t = data['EM_data'][q]['time_steps']
-
-    XR_est = XY_est[:, 0]
-    YR_est = XY_est[:, 1]
 
 
-    dx = np.mean(XR[0:t] - XR_est)
-    dy = np.mean(YR[0:t] - YR_est)
+class DataAnalyzer:
+    def __init__(self, filename):
+        """
+        filename - filename containing data file from EM run
+        Loads in data from file and saves parameters in class
+        """
+        
+        self.data = pkl.load(open(filename, 'rb'))
+
+        self.XR = self.data['XR'][0]
+        self.YR = self.data['YR'][0]
+        self.S = self.data['S']
+
+        # Convert retinal positions to grid
+        XS = self.data['XS']
+        YS = self.data['YS']
+
+        XS, YS = np.meshgrid(XS, YS)
+        self.XS = XS.ravel()
+        self.YS = YS.ravel()
+
+        self.N_itr = self.data['N_itr']
+        self.var = self.data['Var'][0]
+
     
 
-    return SNR(S.ravel(), XS, YS, S_est.ravel(), XS + dx, YS + dy, var)
+    def SNR_idx(self, q):
+        """
+        Calculates the SNR between the ground truth and the 
+            image produced after iteration q of the EM
+        Note that we shift the image estimate by the average
+            amount that the path estimate was off the true path
+            (There is a degeneracy in the representation that this
+            fixes. )
+        """
+        XYR_est = self.data['EM_data'][q]['path_means']
+        S_est = self.data['EM_data'][q]['image_est']
+        t = self.data['EM_data'][q]['time_steps']
 
-SNRs = [SNR_idx(i) for i in range(N_itr)]
+        XR_est = XYR_est[:, 0]
+        YR_est = XYR_est[:, 1]
 
-plt.plot(SNRs)
-plt.show()
+
+        dx = np.mean(self.XR[0:t] - XR_est)
+        dy = np.mean(self.YR[0:t] - YR_est)
+    
+
+        return SNR(self.S.ravel(), self.XS, self.YS, 
+                   S_est.ravel(), self.XS + dx, self.YS + dy, 
+                   self.var)
+
+
+if __name__ == '__main__':
+    da = DataAnalyzer(sys.argv[1])
+
+    print [da.SNR_idx(i) for i in range(da.N_itr)]
+
+#plt.plot(SNRs)
+#plt.show()
