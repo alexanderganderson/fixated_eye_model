0e855c5fd0c3eae9e65ad823c9b75cb42f44d957
Alex Anderson <aga@berkeley.edu>
2016-02-04 12:54:05 -0800

    updated parameters

--- tests/2_comparing_model_assumptions.py
+++ tests/2_comparing_model_assumptions.py
@@ -29,7 +29,7 @@ ig.normalize()
 s_gen = ig.img
 s_gen_name = ig.img_name
 
-n_t = 100
+n_t = 50
 LAMBDA = 0.0
 
 # motion_gen0 = {'mode': 'Diffusion', 'dc': 100.}
@@ -39,7 +39,7 @@ motion_gen = {'mode': 'Experiment', 'fpath': 'data/paths.mat'}
 motion_prior0 = {'mode': 'PositionDiffusion', 'dc': 0.00001}
 motion_prior1 = {'mode': 'PositionDiffusion', 'dc': 20.}
 motion_prior2 = {'mode': 'VelocityDiffusion',
-                 'v0': np.sqrt(40.), 'dcv': 50.}
+                 'v0': np.sqrt(40.), 'dcv': 100.}
 
 modes = []
 # Motion Prior: no motion, Image Prior: Independent Pixels
@@ -62,9 +62,9 @@ modes.append((motion_prior2, D1))
 for motion_prior, D in modes:
     emb = EMBurak(
         s_gen, D, motion_gen, motion_prior, n_t=n_t, save_mode=True,
-        s_gen_name=s_gen_name, ds=1.0, neuron_layout='hex',
+        s_gen_name=s_gen_name, ds=0.8, neuron_layout='hex',
         de=1.09, l_n=6, n_itr=10, lamb=0.0, tau=0.05)
-    for _ in range(1):
+    for _ in range(10):
         XR, YR, R = emb.gen_data(s_gen, print_mode=False)
         emb.run_EM(R)
         emb.save()
