3287bdedcc7a2e2d1ec206d9a9ed968170a51da0
Alex Anderson <aga@berkeley.edu>
2016-04-20 17:44:33 -0700

    made parameter cross validation iterate over all variables

--- tests/5_choosing_parameters.py
+++ tests/5_choosing_parameters.py
@@ -54,23 +54,22 @@ gamma_ = [1., 10., 100.]
 lamb_ = [0., 0.01, 0.1]
 fista_c_ = [0.3, 0.8, 1.5]
 n_g_itr_ = [20, 40, 60]
-n_itr_ = [5, 10, 20]
+n_itr_ = [10, 20, 50]
 tau_ = [0.05, 0.2, 0.5]
 n_p_ = [5, 20, 40]
 
-
-param_ = n_g_itr_
-param_name = 'n_g_itr'
-output_dir = join('parameter_cv', param_name)
-
-
-for param in param_:
-    emb = EMBurak(
-        s_gen, D, motion_gen, motion_prior, n_t=n_t, save_mode=True,
-        s_gen_name=s_gen_name, ds=0.57, neuron_layout='hex',
-        de=1.09, l_n=6, print_mode=False, output_dir_base=output_dir, **{param_name: param})
-    for _ in range(n_repeats):
-        XR, YR, R = emb.gen_data(s_gen)
-        emb.run_em(R)
-        emb.save()
-        emb.reset()
+param__ = [gamma_, lamb_, fista_c_, n_g_itr_, n_itr_, tau_, n_p_]
+param_name_ = ['gamma', 'lamb', 'fista_c', 'n_g_itr', 'n_itr', 'tau', 'n_p']
+for param_, param_name in zip(param__, param_name_):
+    output_dir = join('parameter_cv', param_name)
+    for param in param_:
+        emb = EMBurak(
+            s_gen, D, motion_gen, motion_prior, n_t=n_t, save_mode=True,
+            s_gen_name=s_gen_name, ds=0.57, neuron_layout='hex',
+            de=1.09, l_n=6, print_mode=False, output_dir_base=output_dir,
+            **{param_name: param})
+        for _ in range(n_repeats):
+            XR, YR, R = emb.gen_data(s_gen)
+            emb.run_em(R)
+            emb.save()
+            emb.reset()
