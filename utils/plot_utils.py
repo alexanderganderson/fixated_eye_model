1848f4d34e254636287227449a3d92f9abf0049d
Alex Anderson <aga@berkeley.edu>
2017-06-12 17:29:43 -0700

    cleaned up natural sparsity plots and added code to plot subplot labels

---
+++ utils/plot_utils.py
@@ -0,0 +1,41 @@
+"""Utilities for Plotting."""
+
+import matplotlib.pyplot as plt
+
+def label_subplot(fig, ax, label, dx=0.005 * 4, dy=0.005):
+    """
+    Add a label with a fixed offset in figure coordinates to a subplot.
+
+    Parameters
+    ----------
+    fig : plt.Figure
+        Figure
+    ax : plt.Axes()
+        Axis corresponding to individual subplot.
+    text : str
+        Label
+    dx, dy : float
+        Amount to translate the letter relative to the top right corner of the subplot.
+    """
+    # Compose transformer:
+    tf = ax.transAxes.inverted() + fig.transFigure
+    # transFigure takes you from [0,1] figure coordinates to pixels
+    # transAxes.inverted takes you from pixels to coordinates of axes
+    # ax.text uses these coordinates
+
+    # Note by default, text uses subplot coordinates, which gives undesirable
+    # behavior of the subplots have different sizes.
+
+    # Transform delta
+    u, v = tf.transform((dx, dy)) - tf.transform((0, 0))
+    ax.text(-u, 1 + v, label, size=8, weight='bold', transform=ax.transAxes)
+
+
+def equalize_y_axes(ax0, ax1):
+    u0, v0 = ax0.get_ylim()
+    u1, v1 = ax1.get_ylim()
+
+    u, v = min(u0, u1), max(v0, v1)
+    for ax in [ax0, ax1]:
+        ax.set_ylim(u, v)
+
